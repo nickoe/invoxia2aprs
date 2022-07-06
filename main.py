34e602bb2d81e31371f8b2b19d1cad67eb74047c
Nick Østergaard <oe.nick@gmail.com>
2022-07-06 23:00:05 +0200

    Refactor a little bit

--- main.py
+++ main.py
@@ -1,23 +1,34 @@
+#!/usr/bin/env python3
 import datetime
 import logging
 import math
 import unittest
-from typing import List
+from typing import List, Optional
 import aprslib
 
 import gps_tracker
+from gps_tracker.client.datatypes import TrackerIcon
+
 import secrets
+
+log = logging.getLogger("main")
+log.setLevel(logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
+
 
 # This is meant to get the position of my invoxia tracker and broadcast it to the ARPS network.
 # Copyright 2022 Nick Østergaard / OZ3RF
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)  # add assertion here
+    def test_icon_detect(self):
+        instance = MyAPRS()
+        # Check we can do a correct conversion
+        self.assertEqual(instance.icon_detect(TrackerIcon.HELICOPTER), ('/', 'X'))
+        # Some TrackerIcon not in the conversion list, i.e. fallback APRS icon
+        self.assertEqual(instance.icon_detect(TrackerIcon.WOMAN), ('/', ':'))
 
-
-class MyTracker():
+class MyTracker:
     def __init__(self):
         self.username = secrets.username
         self.password = secrets.password
@@ -28,34 +39,27 @@ class MyTracker():
         trackers: List[gps_tracker.Tracker] = client.get_devices(kind="tracker")
         tracker: gps_tracker.Tracker = trackers[0]
         # location = client.get_locations(tracker, not_before=datetime.datetime.now() - datetime.timedelta(minutes=15), max_count=1)
-        location = client.get_locations(tracker, max_count=1)[0]  # TODO: Assumes we only have on tracker
+        location = client.get_locations(tracker, max_count=1)[0]  # TODO: Assumes we only have one tracker
         now = datetime.datetime.now(datetime.timezone.utc)
         difftime = now - location.datetime.replace()
-        print(f"\t{tracker.tracker_status.battery} % battery")
-        print(f"\t{now} is now")
-        print(f"\t{location.datetime} which was {difftime} ago")
-        print(f"\t{location.lat} {location.lng}")
-        return (location.lat, location.lng, location.datetime, tracker.tracker_status.battery)
+        log.info(f"\t{tracker.tracker_status.battery} % battery")
+        log.info(f"\t{now} is now")
+        log.info(f"\t{location.datetime} which was {difftime} ago")
+        log.info(f"\t{location.lat} {location.lng}")
+        #return (location.lat, location.lng, location.datetime, tracker.tracker_status.battery)
+        return location, tracker
 
 
-class MyAPRS():
+class MyAPRS:
     def __init__(self):
-        # frame = aprs.parse_frame('OZ3RF>APRS:>Hello World!')
-        # a = aprs.TCP(b'OZ3RF', b'18922')
-        # a.start()
-        # a.send(frame)
         self.callsign = secrets.callsign
         self.passcode = aprslib.passcode(self.callsign)
-        logging.basicConfig(level=logging.DEBUG)  # level=10
-        self.AIS = aprslib.IS(self.callsign, passwd=self.passcode)
-        #self.IS = aprslib.IS("N0CALL")
+        self.AIS = aprslib.IS(callsign=self.callsign, passwd=self.passcode)
+        self.posreport: Optional[str] = None
 
-    def deg_to_dms(self, deg, type='lat'):
+    def deg_to_dmh(self, deg, type='lat'):
         """
         Converts signed decimal degrees to degree-minutes-hundredths compatible with the APRS spec ddmmhh notation.
-        :param deg:
-        :param type:
-        :return:
         """
         decimals, number = math.modf(deg)
         d = int(number)
@@ -64,8 +68,8 @@ class MyAPRS():
         #hundredths of minutes
         h = ( s / 60 * 100)
         compass = {
-            'lat': ('N','S'),
-            'lon': ('E','W')
+            'lat': ('N', 'S'),
+            'lon': ('E', 'W')
         }
         compass_str = compass[type][0 if d >= 0 else 1]
         digits = {
@@ -76,35 +80,62 @@ class MyAPRS():
         out = '{:0>{deg_digits}}{:02}.{:02}{}'.format(abs(d), abs(m), int(abs(h)), compass_str, deg_digits=deg_digits)
         return out
 
-    def posdeg_to_pos_dms(self, pos):
-        lat = self.deg_to_dms(pos[0], type='lat')
-        lon = self.deg_to_dms(pos[1], type='lon')
-        return (lat, lon)
+    def posdeg_to_posdmh(self, in_lat, in_lon):
+        lat = self.deg_to_dmh(in_lat, type='lat')
+        lon = self.deg_to_dmh(in_lon, type='lon')
+        return lat, lon
 
+    def icon_detect(self, ti: TrackerIcon):
+        icon_table = {
+            TrackerIcon.BIKE: ('/', 'b'),
+            TrackerIcon.HELICOPTER: ('/', 'X'),
+            TrackerIcon.BACKPACK: ('/', '"'),
+            TrackerIcon.TENT: ('/', ';'),
+            TrackerIcon.ANTENNA: ('/', 'r'),
+        }
+        if ti in icon_table:
+            return icon_table[ti]
+        else:
+            # Fallback icon
+            return '/', ':'
 
-    def callback(self, packet):
-        print(packet)
-
-    def thinghere(self, pos):
-        self.AIS.connect()
-        #self.AIS.consumer(self.callback, raw=False)
-        #self.AIS.sendall("OZ3RF>APRS42,TCPIP*:>Hello World! With aprslib.")
-        dmspos = self.posdeg_to_pos_dms(pos)
-        dt = pos[2]
+    def create_position_msg(self, location: gps_tracker.TrackerData, tracker: gps_tracker.Tracker, msg: str = 'Sigfox tracker (TESTING)'):
+        """
+        This takes location and tracker information to generate an appropriate position message.
+        """
+        dmhpos = self.posdeg_to_posdmh(location.lat, location.lng)
+        dt = location.datetime
         hhmmss = datetime.datetime.strftime(dt, "%H%M%S")
-        batpct = pos[3]
-        posreport = f"OZ3RF>APRS,TCPIP*:/{hhmmss}h{dmspos[0]}/{dmspos[1]}bSigfox tracker (TESTING) Battery: {batpct}%"
-        print(posreport)
-        parsed = aprslib.parse(posreport)
-        print(parsed)
-        self.AIS.sendall(posreport)
-        print("Done")
+        primary, alternate = self.icon_detect(tracker.tracker_config.icon)
+        batpct = tracker.tracker_status.battery
+        self.posreport = f"{self.callsign}>APRS,TCPIP*:/{hhmmss}h{dmhpos[0]}{primary}{dmhpos[1]}{alternate}{msg} [Battery: {batpct}%]"
+
+        log.debug(self.posreport)
+        log.debug(aprslib.parse(self.posreport))
+
+    def broadcast(self):
+        self.AIS.connect()
+        self.AIS.sendall(self.posreport)
+
+
+class SuperClass:
+    def __init__(self):
+        self.tracker: Optional[gps_tracker.Tracker] = None
+        self.location: Optional[gps_tracker.TrackerData] = None
+        self.fetch_data()
+        self.broadcast_data()
+
+    def fetch_data(self):
+        trk_cls = MyTracker()
+        self.location, self.tracker = trk_cls.get_something()
+
+    def broadcast_data(self):
+        aprs_cls = MyAPRS()
+        aprs_cls.create_position_msg(self.location, self.tracker)
+        aprs_cls.broadcast()
 
 
 if __name__ == '__main__':
-    # unittest.main()
-    x = MyTracker()
-    pos = x.get_something()
-    a = MyAPRS()
-    a.thinghere(pos)
-    pass
+    #unittest.main()
+    SuperClass()
+    log.info("Done.")
